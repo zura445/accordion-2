1e5f9d5ddf50f591e15cc1e7c0cece7082c757a5
zura tetrashvili <zuratetra@gmail.com>
2024-12-15 16:42:34 +0400

    add onClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,89 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [active, setActive] = useState(0);
+  console.log(active);
+
   return (
     <>
-      <div className="">sdvcdsc</div>
+      <div className="">
+        <div className="">
+          <h1
+            className="cursor-pointer"
+            onClick={() => {
+              if (active === 0) {
+                setActive(1);
+              } else {
+                setActive(0);
+              }
+            }}
+          >
+            Question
+          </h1>
+          {active === 1 ? <p>Answer</p> : null}
+        </div>
+        <div className="">
+          <h1
+            className="cursor-pointer"
+            onClick={() => {
+              if (active === 0) {
+                setActive(2);
+              } else {
+                setActive(0);
+              }
+            }}
+          >
+            Question
+          </h1>
+          {active === 2 ? <p>Answer</p> : null}
+        </div>
+        <div className="">
+          <h1
+            className="cursor-pointer"
+            onClick={() => {
+              if (active === 0) {
+                setActive(3);
+              } else {
+                setActive(0);
+              }
+            }}
+          >
+            Question
+          </h1>
+          {active === 3 ? <p>Answer</p> : null}
+        </div>
+        <div className="">
+          <h1
+            className="cursor-pointer"
+            onClick={() => {
+              if (active === 0) {
+                setActive(4);
+              } else {
+                setActive(0);
+              }
+            }}
+          >
+            Question
+          </h1>
+          {active === 4 ? <p>Answer</p> : null}
+        </div>
+        <div className="">
+          <h1
+            className="cursor-pointer"
+            onClick={() => {
+              if (active === 0) {
+                setActive(5);
+              } else {
+                setActive(0);
+              }
+            }}
+          >
+            Question
+          </h1>
+          {active === 5 ? <p>Answer</p> : null}
+        </div>
+      </div>
     </>
   );
 }
